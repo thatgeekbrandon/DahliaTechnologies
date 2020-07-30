6e74551594047b7a1ff4b657f6d24eeb83b41b2a
thatgeekbrandon <68824480+thatgeekbrandon@users.noreply.github.com>
2020-07-30 17:48:10 -0600

    Add initial project files

---
+++ POSSystem/POSSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Dahlia Technologies";
+            string tagline = "Architects of Innovation";
+
+
+
+            Console.WriteLine(greeting);
+            Console.WriteLine(tagline);
+        }
+    }
+}
